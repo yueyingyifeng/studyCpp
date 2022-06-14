23fc5bef08e54868f1c72bdf8aa16d2de484b8bd
yyyf <yueyingyifeng@gmail.com>
2022-06-14 10:00:58 +0800

    拆包与装包

---
+++ studyC++/study/object_study/object.cpp
@@ -0,0 +1,45 @@
+//2022年6月14日
+//装包和拆包
+
+#include <iostream>
+using namespace std;
+
+class Object{
+public:
+    virtual string toString(){
+        return "";
+    }
+};
+
+class A
+    : public Object{
+protected:
+    int a;
+    string str;
+public:
+    A(int a,string str){
+        this->a = a;
+        this->str = str;
+    }
+    int getA(){
+        return a;
+    }
+    string getStr(){
+        return str;
+    }
+};
+
+
+void fun(Object* o){
+    A* a = (A*)o;
+
+    cout << a->getA() << endl;
+    cout << a->getStr() << endl;
+
+}
+
+// int main(){
+//     A* a = new A(2,"hi");
+    
+//     fun(a);
+// }
